293a63b02bfc270220dbc97f1cfd4dcc0d9d43db
sschw <sandro.schwager@students.fhnw.ch>
2018-12-13 14:02:51 +0100

    Added Distance Calculation using Chord Length

--- SCLib/Gift.cs
+++ SCLib/Gift.cs
@@ -1,4 +1,6 @@
-﻿namespace SCLib
+﻿using System;
+
+namespace SCLib
 {
     public struct Gift
     {
@@ -6,13 +8,19 @@
         public readonly double Latitute;
         public readonly double Longitude;
         public readonly double Weight;
+        public readonly double X, Y, Z;
 
         public Gift(uint id, double latitute, double longitude, double weight)
         {
             Id = id;
-            Latitute = latitute;
-            Longitude = longitude;
+            // Use radians.
+            Latitute = latitute * Math.PI / 180;
+            Longitude = longitude * Math.PI / 180;
             Weight = weight;
+            // We precalc x, y, z.
+            X = 0.5 * Math.Cos(Latitute) * Math.Sin(Longitude);
+            Y = 0.5 * Math.Cos(Latitute) * Math.Cos(Longitude);
+            Z = 0.5 * Math.Sin(Latitute);
         }
     }
 }
---
+++ SCLib/Utils.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SCLib
+{
+    static class Utils
+    {
+        public static double CalcPenalty(List<int> trip, List<Gift> gifts, double weightSum) {
+            if (trip.Count == 0) return 0.0;
+            double res = 0.0;
+            double w = weightSum + 10.0;
+            for (int i=0; i<trip.Count-1; i++) {
+                res += CalcDistance(gifts, trip[i], trip[i + 1]) * w;
+                w -= gifts[trip[i + 1]].Weight;
+            }
+            return res;
+        }
+
+        public static double CalcDistance(List<Gift> gifts, int gift1, int gift2)
+        {
+            // We use Chord Length as we have less cos and sin calls.
+            double dx = gifts[gift1].X - gifts[gift2].X;
+            double dy = gifts[gift1].Y - gifts[gift2].Y;
+            double dz = gifts[gift1].Z - gifts[gift2].Z;
+            double d = dx * dx + dy * dy + dz * dz;
+            double r = Math.Sqrt(d);
+            // We lose to much precision. We normalize the values to still be accurate.
+            if (r < 0.02) return r * (2 + (2.0 / 6) * d);
+            return 2 * Math.Asin(r);
+        }
+    }
+}
